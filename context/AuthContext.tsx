808602b6be2c9bf81948bc50cdb75528cffa4325
Akash raj <aakashrajgic@gmail.com>
2025-07-08 10:41:01 +0530

    firebase error fixed it

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,7 +1,10 @@
-import { User } from "firebase/auth";
+// import { User } from "firebase/auth";
 import { createContext } from "react";
+
+
 interface AuthContextType {
-    user: User | null;
+    user: any | null;
+    // user: User | null;
 }
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
